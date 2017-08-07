eac4b5a934aff0125e331d88493edcacd1a76817
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2017-08-07 12:05:37 +0200

    Add doctests for the `offset_of` macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+/// # Examples
+///
+/// ```rust
+/// # #[macro_use] extern crate bytes_of;
+/// struct Foo {
+///     bar: Bar,
+///     baz: i32,
+/// }
+///
+/// struct Bar {
+///     quux: i64,
+///     norf: i8,
+/// }
+///
+/// # fn main() {
+/// assert_eq!(offset_of!(Foo, bar), 0);
+/// assert_eq!(offset_of!(Foo, baz), std::mem::size_of::<Bar>());
+/// assert_eq!(offset_of!(Foo, bar.quux), 0);
+/// assert_eq!(offset_of!(Foo, bar.norf), 8);
+/// # }
+/// ```
+#[macro_export]
 macro_rules! offset_of {
     ($T:ty, $($field:ident).+) => {
         unsafe { &((*(0 as *const $T)).$($field).+) as *const _ as usize }
